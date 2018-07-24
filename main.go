b1f8b54f37acbe8e8bf087f2b48fca7405552297
losfair <zhy20000919@hotmail.com>
2018-07-25 01:48:08 +0800

    main: Imports & delegates

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"fmt"
 	"io/ioutil"
@@ -8,15 +9,30 @@ import (
 )
 
 func main() {
+	entryFunctionIDFlag := flag.Int("entry", 0, "entry function id")
+	flag.Parse()
+
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 
-	vm := exec.NewVirtualMachine(input)
-	vm.Ignite(0)
+	vm := exec.NewVirtualMachine(input, func(module, field string) exec.FunctionImport {
+		fmt.Printf("Resolve: %s %s\n", module, field)
+		if module == "env" && field == "__life_ping" {
+			return func(vm *exec.VirtualMachine) int64 {
+				return vm.GetCurrentFrame().Locals[0] + 1
+			}
+		}
+		panic("unknown import")
+	})
+	vm.Ignite(*entryFunctionIDFlag)
 	for !vm.Exited {
 		vm.Execute()
+		if vm.Delegate != nil {
+			vm.Delegate()
+			vm.Delegate = nil
+		}
 	}
 	if vm.ExitError != nil {
 		panic(vm.ExitError)
