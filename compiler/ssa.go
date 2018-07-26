89ced5c7c06ef3118209869451e801fe1edaff59
losfair <zhy20000919@hotmail.com>
2018-07-27 01:43:10 +0800

    compiler/ssa: Add rem ops

--- compiler/ssa.go
+++ compiler/ssa.go
@@ -181,9 +181,9 @@ func (c *SSAFunctionCompiler) Compile() {
 			c.Code = append(c.Code, buildInstr(retID, ins.Op.Name, []int64{int64(math.Float64bits(ins.Immediates[0].(float64)))}, nil))
 			c.PushStack(retID)
 
-		case "i32.add", "i32.sub", "i32.mul", "i32.div_s", "i32.div_u", "i32.and", "i32.or", "i32.xor", "i32.shl", "i32.shr_s", "i32.shr_u", "i32.rotl", "i32.rotr",
+		case "i32.add", "i32.sub", "i32.mul", "i32.div_s", "i32.div_u", "i32.rem_s", "i32.rem_u", "i32.and", "i32.or", "i32.xor", "i32.shl", "i32.shr_s", "i32.shr_u", "i32.rotl", "i32.rotr",
 			"i32.eq", "i32.ne", "i32.lt_s", "i32.lt_u", "i32.le_s", "i32.le_u", "i32.gt_s", "i32.gt_u", "i32.ge_s", "i32.ge_u",
-			"i64.add", "i64.sub", "i64.mul", "i64.div_s", "i64.div_u", "i64.and", "i64.or", "i64.xor", "i64.shl", "i64.shr_s", "i64.shr_u", "i64.rotl", "i64.rotr",
+			"i64.add", "i64.sub", "i64.mul", "i64.div_s", "i64.div_u", "i64.rem_s", "i64.rem_u", "i64.and", "i64.or", "i64.xor", "i64.shl", "i64.shr_s", "i64.shr_u", "i64.rotl", "i64.rotr",
 			"i64.eq", "i64.ne", "i64.lt_s", "i64.lt_u", "i64.le_s", "i64.le_u", "i64.gt_s", "i64.gt_u", "i64.ge_s", "i64.ge_u",
 			"f32.add", "f32.sub", "f32.mul", "f32.div", "f32.min", "f32.max", "f32.copysign",
 			"f32.eq", "f32.ne", "f32.lt", "f32.le", "f32.gt", "f32.ge",
