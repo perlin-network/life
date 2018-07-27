59577e433cd127cb530d0d6729b978fb8bcd8fb7
losfair <zhy20000919@hotmail.com>
2018-07-27 21:08:41 +0800

    spec/test_runner: Support more cases

--- spec/test_runner/runner.go
+++ spec/test_runner/runner.go
@@ -29,13 +29,16 @@ type Command struct {
 	Type string `json:"type"`
 	Line int `json:"line"`
 	Filename string `json:"filename"`
+	Name string `json:"name"`
 	Action CmdAction `json:"action"`
 	Text string `json:"text"`
 	ModuleType string `json:"module_type"`
+	Expected []ValueInfo `json:"expected"`
 }
 
 type CmdAction struct {
 	Type string `json:"type"`
+	Module string `json:"module"`
 	Field string `json:"field"`
 	Args []ValueInfo `json:"args"`
 	Expected []ValueInfo `json:"expected"`
@@ -62,6 +65,7 @@ func LoadConfigFromFile(filename string) *Config {
 
 func (c *Config) Run(cfgPath string) error {
 	var vm *exec.VirtualMachine
+	namedVMs := make(map[string]*exec.VirtualMachine)
 
 	dir, _ := filepath.Split(cfgPath)
 
@@ -72,18 +76,31 @@ func (c *Config) Run(cfgPath string) error {
 			if err != nil {
 				panic(err)
 			}
-			vm, err = exec.NewVirtualMachine(input, exec.VMConfig{
+			localVM, err := exec.NewVirtualMachine(input, exec.VMConfig{
 				MaxMemoryPages: 1024, // for memory trap tests
 			}, &Resolver{})
 			if err != nil {
 				panic(err)
 			}
+			vm = localVM
+			if cmd.Name != "" {
+				namedVMs[cmd.Name] = localVM
+			}
 		case "assert_return", "action":
+			localVM := vm
+			if cmd.Action.Module != "" {
+				if target, ok := namedVMs[cmd.Action.Module]; ok {
+					localVM = target
+				} else {
+					panic("named module not found")
+				}
+			}
+
 			switch cmd.Action.Type {
 			case "invoke":
-				entryID, ok := vm.GetFunctionExport(cmd.Action.Field)
+				entryID, ok := localVM.GetFunctionExport(cmd.Action.Field)
 				if !ok {
-					panic("export not found")
+					panic("export not found (func)")
 				}
 				args := make([]int64, 0)
 				for _, arg := range cmd.Action.Args {
@@ -92,16 +109,35 @@ func (c *Config) Run(cfgPath string) error {
 					args = append(args, val)
 				}
 				fmt.Printf("Entry = %d\n", entryID)
-				ret, err := vm.Run(entryID, args...)
+				ret, err := localVM.Run(entryID, args...)
 				if err != nil {
 					panic(err)
 				}
 				if len(cmd.Action.Expected) != 0 {
 					var exp int64
 					fmt.Sscanf(cmd.Action.Expected[0].Value, "%d", &exp)
+					if cmd.Action.Expected[0].Type == "i32" || cmd.Action.Expected[0].Type == "f32" {
+						ret = int64(uint32(ret))
+						exp = int64(uint32(exp))
+					}
 					if ret != exp {
 						panic(fmt.Errorf("ret mismatch: got %d, expected %d\n", ret, exp))
 					}
+				}
+			case "get":
+				globalID, ok := localVM.GetGlobalExport(cmd.Action.Field)
+				if !ok {
+					panic("export not found (global)")
+				}
+				val := localVM.Globals[globalID]
+				var exp int64
+				fmt.Sscanf(cmd.Expected[0].Value, "%d", &exp)
+				if cmd.Expected[0].Type == "i32" || cmd.Expected[0].Type == "f32" {
+					val = int64(uint32(val))
+					exp = int64(uint32(exp))
+				}
+				if val != exp {
+					panic(fmt.Errorf("val mismatch: got %d, expected %d\n", val, exp))
 				}
 			default:
 				panic(cmd.Action.Type)
