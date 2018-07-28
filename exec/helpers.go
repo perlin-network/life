d6d4f1cce416aad7019e6491c809f49ec6a9d4fc
losfair <zhy20000919@hotmail.com>
2018-07-28 12:18:55 +0800

    exec/helpers: Add NopResolver

--- exec/helpers.go
+++ exec/helpers.go
@@ -5,6 +5,18 @@ import (
 	"github.com/perlin-network/life/utils"
 )
 
+var _ ImportResolver = (*NopResolver)(nil)
+
+type NopResolver struct {}
+
+func (r *NopResolver) ResolveFunc(module, field string) FunctionImport {
+	panic("func import not allowed")
+}
+
+func (r *NopResolver) ResolveGlobal(module, field string) int64 {
+	panic("global import not allowed")
+}
+
 // Returns an error if any happened during execution of user code.
 // Panics on logical errors.
 func (vm *VirtualMachine) RunWithGasLimit(entryID, limit int, params... int64) (int64, error) {
