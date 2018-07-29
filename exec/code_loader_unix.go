c6f773672ebe88cdc2040258e0793a525ae798bc
losfair <zhy20000919@hotmail.com>
2018-07-29 23:21:09 +0800

    exec/code_loader: Remove memfd & enable cc optimizations

--- exec/code_loader_unix.go
+++ exec/code_loader_unix.go
@@ -4,10 +4,6 @@ package exec
 #cgo LDFLAGS: -ldl
 #include <dlfcn.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-
-#define __NR_memfd_create 319 // https://code.woboq.org/qt5/include/asm/unistd_64.h.html
 
 typedef long long i64;
 typedef int i32;
@@ -26,9 +22,6 @@ typedef i32 (*EntryFunc)(
 	i64 *ret
 );
 
-static int memfd_create(const char *name, unsigned int flags) {
-	return syscall(__NR_memfd_create, name, flags);
-}
 static i32 invoke_entry(
 	void *entry,
 	i64 *regs,
@@ -56,27 +49,23 @@ static i32 invoke_entry(
 import "C"
 
 import (
-	"fmt"
 	"bytes"
 	"os/exec"
-	"encoding/base64"
-	"crypto/rand"
 	"unsafe"
 	"os"
 	"runtime"
+	"io/ioutil"
 )
 
 var entryName = C.CString("run")
 
 type DynamicModule struct {
-	shmFd C.int
 	dlHandle unsafe.Pointer
 	entry unsafe.Pointer
 }
 
 func (m *DynamicModule) unsafeCleanup() {
 	C.dlclose(m.dlHandle)
-	C.close(m.shmFd)
 }
 
 func (m *DynamicModule) Run(vm *VirtualMachine, ret *int64) int32 {
@@ -113,53 +102,42 @@ func (m *DynamicModule) Run(vm *VirtualMachine, ret *int64) int32 {
 }
 
 func CompileDynamicModule(source string) *DynamicModule {
-	cmd := exec.Command("cc", "-x", "c", "-shared", "-o", "/dev/stdout", "-")
+	tempFile, err := ioutil.TempFile("", "life-jit")
+	if err != nil {
+		panic(err)
+	}
+	tempFileName := tempFile.Name()
+	tempFile.Close()
+
+	defer os.Remove(tempFileName)
+
+	cmd := exec.Command("cc", "-O1", "-fPIC", "-x", "c", "-shared", "-o", tempFileName, "-")
 	cmd.Stdin = bytes.NewReader([]byte(source))
-	out, err := cmd.Output()
+	_, err = cmd.Output()
 	if err != nil {
-		panic(err)
+		err := err.(*exec.ExitError)
+		if err.Stderr != nil {
+			panic(string(err.Stderr))
+		} else {
+			panic(err)
+		}
 	}
 
-	shmNameRaw := make([]byte, 16)
-	_, err = rand.Read(shmNameRaw)
-	if err != nil {
-		panic(err)
-	}
-	shmName := base64.StdEncoding.EncodeToString(shmNameRaw)
-
-	shmNameC := C.CString(shmName)
-	shmFd := C.memfd_create(shmNameC, 1)
-	C.free(unsafe.Pointer(shmNameC))
-
-	if shmFd < 0 {
-		panic("unable to create shm fd")
-	}
-
-	n := C.write(shmFd, unsafe.Pointer(&out[0]), C.ulong(len(out)))
-	if n < 0 {
-		C.close(shmFd)
-		panic("write failed")
-	}
-
-	shmPath := fmt.Sprintf("/proc/%d/fd/%d", os.Getpid(), shmFd)
-	shmPathC := C.CString(shmPath)
-	dlHandle := C.dlopen(shmPathC, C.RTLD_LAZY)
-	C.free(unsafe.Pointer(shmPathC))
+	tempFilePathC := C.CString(tempFileName)
+	dlHandle := C.dlopen(tempFilePathC, C.RTLD_LAZY)
+	C.free(unsafe.Pointer(tempFilePathC))
 
 	if dlHandle == nil {
-		C.close(shmFd)
 		panic("dlopen failed")
 	}
 
 	entry := C.dlsym(dlHandle, entryName)
 	if entry == nil {
 		C.dlclose(dlHandle)
-		C.close(shmFd)
 		panic("dlsym failed")
 	}
 
 	dm := &DynamicModule {
-		shmFd: shmFd,
 		dlHandle: dlHandle,
 		entry: entry,
 	}
