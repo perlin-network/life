56e1c2d5860f7254fa9004eaecc33f8332f17083
losfair <zhy20000919@hotmail.com>
2018-07-30 23:11:12 +0800

    main: Remove emscripten polyfill

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"github.com/perlin-network/life/exec"
 	"io/ioutil"
-	"math"
-	"strings"
 	"time"
 )
 
@@ -16,88 +14,19 @@ type Resolver struct {
 
 func (r *Resolver) ResolveFunc(module, field string) exec.FunctionImport {
 	fmt.Printf("Resolve func: %s %s\n", module, field)
-	if module != "env" {
-		panic("module != env")
-	}
+	switch module {
+	case "env":
+		switch field {
+		case "__life_ping":
+			return func(vm *exec.VirtualMachine) int64 {
+				return vm.GetCurrentFrame().Locals[0] + 1
+			}
 
-	switch field {
-	case "__life_ping":
-		return func(vm *exec.VirtualMachine) int64 {
-			return vm.GetCurrentFrame().Locals[0] + 1
+		default:
+			panic(fmt.Errorf("unknown field: %s", field))
 		}
-	case "enlargeMemory":
-		return func(vm *exec.VirtualMachine) int64 {
-			panic("enlargeMemory not implemented")
-		}
-
-	case "getTotalMemory":
-		return func(vm *exec.VirtualMachine) int64 {
-			return int64(len(vm.Memory))
-		}
-
-	case "abortOnCannotGrowMemory":
-		return func(vm *exec.VirtualMachine) int64 {
-			panic("Cannot grow memory")
-		}
-
-	case "abortStackOverflow":
-		return func(vm *exec.VirtualMachine) int64 {
-			panic("Emscripten stack overflow")
-		}
-
-	case "___lock", "___unlock":
-		return func(vm *exec.VirtualMachine) int64 {
-			return 0
-		}
-
-	case "___setErrNo":
-		return func(vm *exec.VirtualMachine) int64 {
-			panic("setErrNo not implemented")
-		}
-
-	case "_emscripten_memcpy_big":
-		return func(vm *exec.VirtualMachine) int64 {
-			frame := vm.GetCurrentFrame()
-			dest := int(frame.Locals[0])
-			src := int(frame.Locals[1])
-			num := int(frame.Locals[2])
-			copy(vm.Memory[dest:], vm.Memory[src:src+num])
-			return int64(dest)
-		}
-
-	case "abort":
-		return func(vm *exec.VirtualMachine) int64 {
-			panic("Emscripten abort")
-		}
-
-	case "setTempRet0":
-		return func(vm *exec.VirtualMachine) int64 {
-			r.tempRet0 = vm.GetCurrentFrame().Locals[0]
-			return 0
-		}
-
-	case "getTempRet0":
-		return func(vm *exec.VirtualMachine) int64 {
-			return r.tempRet0
-		}
-
 	default:
-		if strings.HasPrefix(field, "nullFunc_") {
-			return func(vm *exec.VirtualMachine) int64 {
-				panic("nullFunc called")
-			}
-		}
-		if strings.HasPrefix(field, "___syscall") {
-			return func(vm *exec.VirtualMachine) int64 {
-				panic(fmt.Errorf("syscall %s not supported", field))
-			}
-		}
-		if strings.HasPrefix(field, "jsCall_") {
-			return func(vm *exec.VirtualMachine) int64 {
-				panic(fmt.Errorf("jsCall %s not supported", field))
-			}
-		}
-		panic(fmt.Errorf("unknown field: %s", field))
+		panic(fmt.Errorf("unknown module: %s", module))
 	}
 }
 
@@ -108,35 +37,6 @@ func (r *Resolver) ResolveGlobal(module, field string) int64 {
 		switch field {
 		case "__life_magic":
 			return 424
-		case "memoryBase":
-			return 0
-		case "tableBase":
-			return 0
-		case "DYNAMICTOP_PTR":
-			return 16
-		case "tempDoublePtr":
-			return 64
-		case "STACK_BASE":
-			return 4096
-		case "STACKTOP":
-			return 4096
-		case "STACK_MAX":
-			return 4096 + 1048576
-		case "ABORT":
-			return 0
-		case "gb":
-			return 1024
-		case "fb":
-			return 0
-		default:
-			panic(fmt.Errorf("unknown field: %s", field))
-		}
-	case "global":
-		switch field {
-		case "NaN":
-			return int64(math.Float64bits(math.NaN()))
-		case "Infinity":
-			return int64(math.Float64bits(math.Inf(1)))
 		default:
 			panic(fmt.Errorf("unknown field: %s", field))
 		}
